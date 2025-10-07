3191c0f2265408c648caf4c159202bfc6ea6a3ed
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-10-08 00:17:05 +0530

    prisma and neon db added and user sync also

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+//@ts-ignore
+import Navbar from "@/components/Navbar";
+
+
+function DashboardPage() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        {/* <WelcomeSection /> */}
+        {/* <MainActions /> */}
+        {/* <ActivityOverview /> */}
+      </div>
+    </>
+  );
+}
+export default DashboardPage;
